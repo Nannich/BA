7411ead5fdc691e2b2d7889041bfff8df46fbd0f
Yannick <yannickspecht2004@gmail.com>
2026-03-27 14:29:38 +0100

    Renaming and extract tde data

--- RScripts/rds_to_csv.R
+++ RScripts/rds_to_csv.R
@@ -12,28 +12,59 @@ FQnorm <- function(counts) {
   return(norm)
 }
 
-
-extract_trajectory_data <- function(data, crv) {
-  # Paul data has to be treated special
-  counts <- if ("counts" %in% names(data)) data$counts else data
+extract_furcating <- function(data, crv) {
+  counts <-  data$counts
   
   # Normalize counts
   counts <- t(as.matrix(counts))
   counts <- FQnorm(counts)
   counts <- t(counts) # Output: Cells x Genes
   
-  # The pcc curve from the cyclic data has to be treated special
-  if (inherits(crv, "SlingshotDataSet")) {
-    pseudotime  <- slingPseudotime(crv, na = FALSE)
-    weights     <- slingCurveWeights(crv)
-  } else {
-    pseudotime  <- crv$lambda
-    weights     <- rep(1, length(pseudotime)) # Because it has only 1 lineage
-  }
+  pseudotime  <- slingPseudotime(crv, na = FALSE)
+  weights     <- slingCurveWeights(crv)
+  tde         <- data$tde_overall
   
-  return(list(counts = counts, pseudotime = pseudotime, weights = weights))
+  return(list(counts = counts, pseudotime = pseudotime, weights = weights, tde = tde))
 }
 
+extract_cyclic <- function(data, crv) {
+  counts <-  data$counts
+  
+  # Normalize counts
+  counts <- t(as.matrix(counts))
+  counts <- FQnorm(counts)
+  counts <- t(counts) # Output: Cells x Genes
+  
+  pseudotime  <- crv$lambda
+  weights     <- rep(1, length(pseudotime)) # Because it has only 1 lineage
+  
+  tde <- data.frame(
+    feature_id = data$feature_info$feature_id,
+    differentially_expressed = !data$feature_info$is_hk # Anything that is not housekeeping is DE
+  )
+  
+  return(list(counts = counts, pseudotime = pseudotime, weights = weights, tde = tde))
+}
+
+extract_paul <- function(data, crv) {
+  counts <- data
+  
+  # Normalize counts
+  counts <- t(as.matrix(counts))
+  counts <- FQnorm(counts)
+  counts <- t(counts) # Output: Cells x Genes
+  
+  pseudotime  <- slingPseudotime(crv, na = FALSE)
+  weights     <- slingCurveWeights(crv)
+  
+  # No tde data available for the Paul dataset since it is a case study
+  tde <- data.frame(
+    feature_id = colnames(counts),
+    differentially_expressed = NA 
+  )
+  
+  return(list(counts = counts, pseudotime = pseudotime, weights = weights, tde = tde))
+}
 
 process_dataset <- function(category, iter = NULL) {
   # Paul dataset does not have multiple simulations
@@ -48,11 +79,19 @@ process_dataset <- function(category, iter = NULL) {
   
   data <- readRDS(data_path)
   crv  <- readRDS(crv_path)
-  res  <- extract_trajectory_data(data, crv)
+  
+  if (category == "bifurcating" || category == "multifurcating") {
+    res  <- extract_furcating(data, crv)
+  } else if (category == "cyclic") {
+    res  <- extract_cyclic(data, crv)
+  } else if (category == "paul") {
+    res  <- extract_paul(data, crv)
+  }
   
   write.csv(res$counts,     file = paste0(instance_dir, "counts.csv"))
   write.csv(res$pseudotime, file = paste0(instance_dir, "pseudotime.csv"))
   write.csv(res$weights,    file = paste0(instance_dir, "weights.csv"))
+  write.csv(res$tde,        file = paste0(instance_dir, "tde.csv"), row.names = FALSE)
 }
 
 categories <- c("bifurcating", "multifurcating", "cyclic")
